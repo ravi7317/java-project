8f183b7a9b26cc2b0f5260564107b151de5702fe
RAVI VERMA <110763613+ravi7317@users.noreply.github.com>
2023-02-23 22:41:43 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Team {
+    private String name;
+    private int points;
+    private int wins;
+    private int draws;
+    private int losses;
+
+    public Team(String name) {
+        this.name = name;
+        this.points = 0;
+        this.wins = 0;
+        this.draws = 0;
+        this.losses = 0;
+    }
+
+   
