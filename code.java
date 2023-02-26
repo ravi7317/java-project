2c9568076fd75e54e4224db066d2286e510560ea
RAVI VERMA <110763613+ravi7317@users.noreply.github.com>
2023-02-26 14:58:46 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,18 +1,18 @@
-import java.util.*;
-
-class Team {
+public class Team {
     private String name;
     private int points;
     private int wins;
-    private int draws;
     private int losses;
-
+    private int draws;
+    private int goalsFor;
+    private int goalsAgainst;
+    
     public Team(String name) {
-        this.name = name;
-        this.points = 0;
-        this.wins = 0;
-        this.draws = 0;
-        this.losses = 0;
+    this.name = name;
+    this.points = 0;
+    this.wins = 0;
+    this.losses = 0;
+    this.draws = 0;
+    this.goalsFor = 0;
+    this.goalsAgainst = 0;
     }
-
-   
