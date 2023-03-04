6a7c81c1c95889903f1e8c2c70b16341e3f79578
RAVI VERMA <110763613+ravi7317@users.noreply.github.com>
2023-03-04 16:04:26 +0530

    Create match.java

---
+++ match.java
@@ -0,0 +1,12 @@
+public class Match {
+    private Team homeTeam;
+    private Team awayTeam;
+    private int homeGoals;
+    private int awayGoals;
+    
+    public Match(Team homeTeam, Team awayTeam, int homeGoals, int awayGoals) {
+    this.homeTeam = homeTeam;
+    this.awayTeam = awayTeam;
+    this.homeGoals = homeGoals;
+    this.awayGoals = awayGoals;
+    
